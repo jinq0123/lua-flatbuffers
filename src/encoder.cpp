7c4b05474a709b2037d9517fc58e61355a17c975
Jin Qing <jinq0123@163.com>
2016-07-20 19:07:32 +0800

    Check depricated.

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -20,9 +20,13 @@ Encoder::Encode(const std::string& sName, const LuaIntf::LuaRef& table)
 	return Build(sName, table);
 }
 
+// Todo: check required fields.
+// Todo: Skip default value.
+
 std::tuple<bool, std::string>
 Encoder::Build(const std::string& sName, const LuaIntf::LuaRef& table)
 {
+	m_nameStack.push(sName);
 	const reflection::Object* pObj = m_vObjects.LookupByKey(sName.c_str());
 	assert(pObj);
 	const auto& vFields = *pObj->fields();
@@ -32,12 +36,20 @@ Encoder::Build(const std::string& sName, const LuaIntf::LuaRef& table)
 		std::string sKey = e.key<std::string>();
 		const reflection::Field* pField = vFields.LookupByKey(sKey.c_str());
 		if (!pField)
+		{
 			return std::make_tuple(false,
 				"illegal field " + GetFullFieldName(sKey));
+		}
+		if (pField->deprecated())
+		{
+			return std::make_tuple(false,
+				"deprecated field " + GetFullFieldName(sKey));
+		}
 
 		LuaIntf::LuaRef value = e.value<LuaIntf::LuaRef>();
 	}
 
+	m_nameStack.pop();
 	// XXX
 	return std::make_tuple(false, "to be implemented");
 }
