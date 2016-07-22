1eb80bd75376c6db3dcd17ae1cd65f680137db0c
Jin Qing <jinq0123@163.com>
2016-07-22 17:57:49 +0800

    Encode.

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -4,6 +4,8 @@
 
 #include <LuaIntf/LuaIntf.h>
 
+#include <unordered_map>
+
 Encoder::Encoder(const reflection::Schema& schema) :
 	m_schema(schema),
 	m_vObjects(*schema.objects())
@@ -11,11 +13,11 @@ Encoder::Encoder(const reflection::Schema& schema) :
 }
 
 std::tuple<bool, std::string>
-Encoder::Encode(const std::string& sName, const LuaIntf::LuaRef& table)
+Encoder::Encode(const std::string& sName, const LuaRef& table)
 {
 	Reset();
 
-	const reflection::Object* pObj = m_vObjects.LookupByKey(sName.c_str());
+	const Object* pObj = m_vObjects.LookupByKey(sName.c_str());
 	assert(pObj);
 	if (!Encode(*pObj, table))  // An offset of 0 means error.
 		return std::make_tuple(false, m_sError);
@@ -29,34 +31,143 @@ Encoder::Encode(const std::string& sName, const LuaIntf::LuaRef& table)
 // Todo: Skip default value.
 
 flatbuffers::uoffset_t
-Encoder::Encode(const reflection::Object& obj,
-	const LuaIntf::LuaRef& table)
+Encoder::Encode(const Object& obj, const LuaRef& table)
 {
 	m_nameStack.push(obj.name()->str());
-	const auto& vFields = *obj.fields();
 
-	for (auto& e : table)
+	// Cache to map before StartTable().
+	Field2Scalar mapScalar;
+	Field2Offset mapOffset;
+	if (!CacheFields(obj, table, mapScalar, mapOffset))
+		return 0;
+
+	flatbuffers::uoffset_t start = m_fbb.StartTable();
+	AddElements(mapScalar);
+	AddOffsets(mapOffset);
+	m_nameStack.pop();
+	// XXX numFields = vFields.size()?
+	return m_fbb.EndTable(start, obj.fields()->size());
+}
+
+bool Encoder::CacheFields(const Object& obj, const LuaRef& table,
+	Field2Scalar& rMapScalar, Field2Offset& rMapOffset)
+{
+	const auto& vFields = *obj.fields();
+	// bool isStruct = obj.is_struct();
+
+	for (const auto& e : table)
 	{
 		std::string sKey = e.key<std::string>();
 		const reflection::Field* pField = vFields.LookupByKey(sKey.c_str());
 		if (!pField)
 		{
 			m_sError = "illegal field " + GetFullFieldName(sKey);
-			return 0;
+			return false;
 		}
 		if (pField->deprecated())
 		{
 			m_sError = "deprecated field " + GetFullFieldName(sKey);
-			return 0;
+			return false;
 		}
 
-		LuaIntf::LuaRef value = e.value<LuaIntf::LuaRef>();
+		LuaRef value = e.value<LuaRef>();
+		const reflection::Type& type = *pField->type();
+		switch (type.base_type())
+		{
+		case reflection::String:
+			// XXX
+			break;
+		case reflection::Vector:
+			// XXX
+			break;
+		case reflection::Obj:
+			rMapOffset[pField] = Encode(*m_vObjects[type.index()], value);
+			break;
+		case reflection::Union:
+			// XXX
+			break;
+		default:
+			rMapScalar[pField] = value;
+			break;
+		}
 	}
+	return true;
+}
 
-	m_nameStack.pop();
-	// XXX
-	m_sError = "to be implemented";
-	return 0;
+void Encoder::AddElements(const Field2Scalar& mapScalar)
+{
+	for (const auto& e : mapScalar)
+	{
+		const reflection::Field* pField = e.first;
+		assert(pField);
+		AddElement(*pField, e.second);
+	}
+}
+
+void Encoder::AddElement(const reflection::Field& field,
+	const LuaRef& elementValue)
+{
+	const reflection::Type& type = *field.type();
+	int64_t defInt = field.default_integer();
+	double defReal = field.default_real();
+	uint16_t offset = field.offset();
+
+	switch (type.base_type())
+	{
+	case reflection::UType:
+	case reflection::Bool:
+	case reflection::UByte:
+		AddElement<uint8_t>(offset, elementValue, defInt);
+		break;
+	case reflection::Byte:
+		AddElement<int8_t>(offset, elementValue, defInt);
+		break;
+	case reflection::Short:
+		AddElement<int16_t>(offset, elementValue, defInt);
+		break;
+	case reflection::UShort:
+		AddElement<uint16_t>(offset, elementValue, defInt);
+		break;
+	case reflection::Int:
+		AddElement<int32_t>(offset, elementValue, defInt);
+		break;
+	case reflection::UInt:
+		AddElement<uint32_t>(offset, elementValue, defInt);
+		break;
+	case reflection::Long:
+		AddElement<int64_t>(offset, elementValue, defInt);
+		break;
+	case reflection::ULong:
+		AddElement<uint64_t>(offset, elementValue, defInt);
+		break;
+	case reflection::Float:
+		AddElement<float>(offset, elementValue, defReal);
+		break;
+	case reflection::Double:
+		AddElement<double>(offset, elementValue, defReal);
+		break;
+	default:
+		assert(false);
+		break;
+	}
+}
+
+void Encoder::AddOffsets(const Field2Offset& mapOffset)
+{
+	for (const auto& e : mapOffset)
+	{
+		const reflection::Field* pField = e.first;
+		assert(pField);
+		flatbuffers::uoffset_t offset = e.second;
+		m_fbb.AddOffset(pField->offset(), flatbuffers::Offset<void>(offset));
+	}
+}
+
+template <typename T>
+inline void Encoder::AddElement(uint16_t offset,
+	const LuaRef& elementValue, T defaultValue)
+{
+	m_fbb.AddElement<T>(offset, elementValue.toValue<T>(), defaultValue);
 }
 
 std::string Encoder::GetFullFieldName(const std::string& sFieldName) const
--- src/encoder.h
+++ src/encoder.h
@@ -2,13 +2,16 @@
 #define LUA_FLATBUFFERS_ENCODER_H_
 
 #include <flatbuffers/flatbuffers.h>
+
 #include <stack>
+#include <unordered_map>
 
 namespace LuaIntf {
 class LuaRef;
 }
 
 namespace reflection {
+struct Field;
 struct Object;
 struct Schema;
 }
@@ -19,13 +22,30 @@ public:
 	explicit Encoder(const reflection::Schema& schema);
 
 public:
+	using LuaRef = LuaIntf::LuaRef;
 	std::tuple<bool, std::string> Encode(
-		const std::string& sName, const LuaIntf::LuaRef& table);
+		const std::string& sName, const LuaRef& table);
 
 private:
 	// Encode recursively. Return 0 and set m_sError if any error.
+	using Object = reflection::Object;
 	flatbuffers::uoffset_t Encoder::Encode(
-		const reflection::Object& obj, const LuaIntf::LuaRef& table);
+		const Object& obj, const LuaRef& table);
+
+	// Cache to map before StartTable().
+	using Field2Scalar = std::unordered_map<
+		const reflection::Field*, LuaRef>;
+	using Field2Offset = std::unordered_map<
+		const reflection::Field*, flatbuffers::uoffset_t>;
+	bool CacheFields(const Object& obj, const LuaRef& table,
+		Field2Scalar& rMapScalar, Field2Offset& rMapOffset);
+	void AddElements(const Field2Scalar& mapScalar);
+	void AddOffsets(const Field2Offset& mapOffset);
+	void AddElement(const reflection::Field& field, const LuaRef& value);
+
+	template <typename T>
+	inline void AddElement(uint16_t offset, const LuaRef& elementValue,
+		T defaultValue);
 
 private:
 	std::string GetFullFieldName(const std::string& sFieldName) const;
@@ -34,10 +54,11 @@ private:
 private:
 	flatbuffers::FlatBufferBuilder m_fbb;
 	const reflection::Schema& m_schema;
-	const flatbuffers::Vector<flatbuffers::Offset<reflection::Object>>& m_vObjects;
+	const flatbuffers::Vector<flatbuffers::Offset<Object>>& m_vObjects;
 	using NameStack = std::stack<std::string>;
 	NameStack m_nameStack;  // For error message.
 	std::string m_sError;  // Encode error.
+
 };  // class Encoder
 
 #endif  // LUA_FLATBUFFERS_ENCODER_H_
