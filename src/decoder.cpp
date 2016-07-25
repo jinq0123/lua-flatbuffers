b30b2ceaf5ea0bf19f2115669873df3dec71dc84
Jin Qing <jinq0123@163.com>
2016-07-25 13:20:15 +0800

    Get fields.

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -12,6 +12,79 @@ Decoder::Decoder(lua_State* state, const reflection::Schema& schema) :
 	assert(L);
 }
 
+template<typename FieldType>
+inline FieldType GetField(const flatbuffers::Table& fbTable,
+	const reflection::Field& field)
+{
+	return fbTable.GetField<FieldType>(field.offset(),
+		static_cast<FieldType>(fdefaultVal));
+}
+
+static void SetField(const flatbuffers::Table& fbTable,
+	const reflection::Field& field,
+	LuaIntf::LuaRef& rLuaTable)
+{
+	if (field.deprecated()) return;
+	const reflection::Type& type = *field.type();
+	const char* pName = field.name()->c_str();
+	assert(pName);
+	uint16_t offset = field.offset();
+
+	using flatbuffers::GetFieldI;
+	using flatbuffers::GetFieldF;
+	switch (type.base_type())
+	{
+	case reflection::UType:
+	case reflection::Bool:
+	case reflection::UByte:
+		rLuaTable[pName] = GetFieldI<uint8_t>(fbTable, field);
+		break;
+	case reflection::Byte:
+		rLuaTable[pName] = GetFieldI<int8_t>(fbTable, field);
+		break;
+	case reflection::Short:
+		rLuaTable[pName] = GetFieldI<int16_t>(fbTable, field);
+		break;
+	case reflection::UShort:
+		rLuaTable[pName] = GetFieldI<uint16_t>(fbTable, field);
+		break;
+	case reflection::Int:
+		rLuaTable[pName] = GetFieldI<int32_t>(fbTable, field);
+		break;
+	case reflection::UInt:
+		rLuaTable[pName] = GetFieldI<uint32_t>(fbTable, field);
+		break;
+	case reflection::Long:
+		rLuaTable[pName] = GetFieldI<int64_t>(fbTable, field);
+		break;
+	case reflection::ULong:
+		rLuaTable[pName] = GetFieldI<uint64_t>(fbTable, field);
+		break;
+	case reflection::Float:
+		rLuaTable[pName] = GetFieldF<float>(fbTable, field);
+		break;
+	case reflection::Double:
+		rLuaTable[pName] = GetFieldF<double>(fbTable, field);
+		break;
+
+	case reflection::String:
+		// XXX
+		break;
+	case reflection::Vector:
+		// XXX
+		break;
+	case reflection::Obj:
+		// XXX
+		break;
+	case reflection::Union:
+		// XXX
+		break;
+	default:
+		assert(false);
+		break;
+	}
+}
+
 std::tuple<LuaIntf::LuaRef, std::string>
 Decoder::Decode(const std::string& sName, const std::string& buf)
 {
@@ -27,64 +100,10 @@ Decoder::Decode(const std::string& sName, const std::string& buf)
 	for (const reflection::Field* pField : *pObj->fields())
 	{
 		assert(pField);
-		if (pField->deprecated()) continue;
-		const reflection::Type& type = *pField->type();
-		const char* pName = pField->name()->c_str();
-		assert(pName);
-		switch (type.base_type())
-		{
-		case reflection::UType:
-		case reflection::Bool:
-		case reflection::UByte:
-			luaTable[pField->name()] = "XXX";
-			AddElement<uint8_t>(offset, elementValue, defInt);
-			break;
-		case reflection::Byte:
-			AddElement<int8_t>(offset, elementValue, defInt);
-			break;
-		case reflection::Short:
-			AddElement<int16_t>(offset, elementValue, defInt);
-			break;
-		case reflection::UShort:
-			AddElement<uint16_t>(offset, elementValue, defInt);
-			break;
-		case reflection::Int:
-			AddElement<int32_t>(offset, elementValue, defInt);
-			break;
-		case reflection::UInt:
-			AddElement<uint32_t>(offset, elementValue, defInt);
-			break;
-		case reflection::Long:
-			AddElement<int64_t>(offset, elementValue, defInt);
-			break;
-		case reflection::ULong:
-			AddElement<uint64_t>(offset, elementValue, defInt);
-			break;
-		case reflection::Float:
-			AddElement<float>(offset, elementValue, defReal);
-			break;
-		case reflection::Double:
-			AddElement<double>(offset, elementValue, defReal);
-			break;
-
-		case reflection::String:
-			// XXX
-			break;
-		case reflection::Vector:
-			// XXX
-			break;
-		case reflection::Obj:
-			// XXX
-			break;
-		case reflection::Union:
-			// XXX
-			break;
-		default:
-			table[pField->name()] = "XXX";
-			break;
-		}
+		SetField(fbTable, *pField, luaTable);
 	}
+
 	// XXX
-	return std::make_tuple(table, "");
+	return std::make_tuple(luaTable, "");
 }
 
