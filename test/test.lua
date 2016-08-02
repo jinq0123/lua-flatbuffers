eeb417b46d0d40e0aa659f5ae0edf7b5c7e4e0bf
Jin Qing <jinq0123@163.com>
2016-08-02 11:15:23 +0800

    Update test.

--- test/test.lua
+++ test/test.lua
@@ -1,14 +1,29 @@
 package.cpath = package.cpath .. ";../bin/Debug/?.dll"
 
 lfb = require("lfb")
+inspect = require("inspect")
+
 assert(lfb.load_bfbs_file("../third_party/flatbuffers/tests/monster_test.bfbs"))
 
-buf = assert(lfb.encode("Monster", {}))
-t = assert(lfb.decode("Monster", buf))
+function test_required()
+	buf = assert(lfb.encode("Monster", {}))
+	t, err = lfb.decode("Monster", buf)
+	assert(err == "illegal required field Monster.name")
+end  -- test_required()
 
-TO_SHORT = "buffer is too short"
-t, err = lfb.decode("Monster", "")
-assert(err == TO_SHORT)
-t, err = lfb.decode("Monster", "123")
-assert(err == TO_SHORT)
-assert(not lfb.decode("Monster", "1234"))
+function test_too_short()
+	TO_SHORT = "buffer is too short"
+	t, err = lfb.decode("Monster", "")
+	assert(err == TO_SHORT)
+	t, err = lfb.decode("Monster", "123")
+	assert(err == TO_SHORT)
+	assert(not lfb.decode("Monster", "1234"))
+	assert(not lfb.decode("Monster", "1234"))
+end
+
+function test_all()
+	test_required()
+	test_too_short()
+end  -- test_all()
+
+test_all()
