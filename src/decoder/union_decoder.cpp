87e08fe5c211a7cc99b5086c37dd0c3775c55495
Jin Qing <jinq0123@163.com>
2016-08-05 14:57:01 +0800

    Verify illegal union type.

--- src/decoder/union_decoder.cpp
+++ src/decoder/union_decoder.cpp
@@ -3,6 +3,8 @@
 #include "object_decoder.h"  // for ObjectDecoder
 #include "vector_decoder.h"  // for VectorDecoder
 
+#include <sstream>  // for ostringstream
+
 LuaIntf::LuaRef UnionDecoder::DecodeUnion(const reflection::Type& type,
 	int64_t nUnionType, const void* pData)
 {
@@ -11,13 +13,22 @@ LuaIntf::LuaRef UnionDecoder::DecodeUnion(const reflection::Type& type,
 	const reflection::Enum* pEnum = (*m_rCtx.schema.enums())[type.index()];
 	assert(pEnum);
 	assert(pEnum->is_union());
+	assert(pEnum->values()->LookupByKey(0));  // Union type: 1..n, 0(Unknown)?
+	assert(pEnum->values()->LookupByKey(0)->object() == nullptr);
+
 	const reflection::EnumVal* pEnumVal =
 		pEnum->values()->LookupByKey(nUnionType);
 	if (!pEnumVal) return Nil();  // Allow schema version compatibility.
 	const reflection::Object* pObj = pEnumVal->object();
-	assert(pObj);
-	// only tables can be union elements
-	assert(!pObj->is_struct());
-	return ObjectDecoder(m_rCtx).DecodeObject(*pObj, pData);
-}
+	if (pObj)
+	{
+		// only tables can be union elements
+		assert(!pObj->is_struct());
+		return ObjectDecoder(m_rCtx).DecodeObject(*pObj, pData);
+	}
 
+	std::ostringstream oss;
+	oss << "illegal union type " << PopFullName()
+		<< flatbuffers::UnionTypeFieldSuffix() << "(" << nUnionType << ")";
+	ERR_RET_NIL(oss.str());
+}
